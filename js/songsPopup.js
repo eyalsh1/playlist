ff92b61be03ce81304ab99bd1c844d6ef467c2fd
Eyal Shalom <eyal.shalom@gmail.com>
2017-07-03 18:06:22 +0300

    Add songs

--- js/songsPopup.js
+++ js/songsPopup.js
@@ -7,27 +7,67 @@ class SongsPopup {
     build() {
         var content = this.form.parent();
         this.form.remove();
-        console.log(content);
-        //console.log(this.newPlaylistObject);
         $(".panel-heading").text("Add Playlist Songs");
-
-        /*$.get('html/addAnotherSong.html', function(data) {
-            content.html(data);
-        });*/
 
         $.get('html/addPlayListSongs.html', function(data) {
             content.html(data);
-        });
 
-        //$("<span>Hello world!</span>").insertAfter(content.find('form fieldset:last-of-type'));
+            content.find('#add-another-song-button').click(function(event) {
+                addSong().insertAfter(content.find('fieldset:last-of-type'));
+            });
 
-        /*$.get('html/addAnotherSong.html', function(data) {
-            $(html(data)).insertAfter("button");
+            content.find('form').submit(function(event) {
+                event.preventDefault();
+                newPlaylistObject.songs = [];
+                $(event.target).find('fieldset').each(function(index, el) {
+                    var song = {};
+                    song.url = $(el).find('input[name=url]').val();
+                    song.name = $(el).find('input[name=name]').val();
+                    newPlaylistObject.songs.push(song);
+                });
+                //console.log(newPlaylistObject);
+                $("#popup_background").remove();
+            });
         });
-        $.get('html/addAnotherSong.html', function(data) {
-            var content = $('<div>', {
-                html: data,
-            }).appendTo(content);
-        });*/
     }
+}
+
+function addSong() {
+    var fieldset = $('<fieldset>');
+
+    var formGroupDiv = $('<div>', {
+        class: "form-group",
+    }).appendTo(fieldset);
+
+    $('<label>', {
+        class: "col-sm-2 control-label",
+        text: "Song URL",
+    }).appendTo(formGroupDiv);
+
+    var colDiv1 = $('<div>', {
+        class: "col-sm-5",
+    }).appendTo(formGroupDiv);
+
+    $('<input>', {
+        type: "url",
+        class: "form-control",
+        name: "url",
+    }).appendTo(colDiv1);
+
+    $('<label>', {
+        class: "col-sm-2 control-label",
+        text: "Name",
+    }).appendTo(formGroupDiv);
+
+    var colDiv2 = $('<div>', {
+        class: "col-sm-3",
+    }).appendTo(formGroupDiv);
+
+    $('<input>', {
+        type: "text",
+        class: "form-control",
+        name: "name",
+    }).appendTo(colDiv2);
+
+    return fieldset;
 }
